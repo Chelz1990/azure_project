7a4aeb750dea1efebefcdb76fe71ee36d2cab929
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-02 08:33:30 -0500

    edited db

--- database.tf
+++ database.tf
@@ -46,10 +46,10 @@ resource "azurerm_mysql_flexible_server" "wordpress" {
     size_gb = 20
   }
 
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.db_dns_link]
+#  depends_on = [azurerm_private_dns_zone_virtual_network_link.db_dns_link]
   
-  provisioner "local-exec" {
-    command = "ansible-playbook wordpress.yml"
-  }
+#  provisioner "local-exec" {
+#    command = "ansible-playbook wordpress.yml"
+#  }
 }
 
