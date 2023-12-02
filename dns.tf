c4f972b5e285d9fe9cce5c5522fae7efefa29490
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-02 08:39:35 -0500

    added dns

--- dns.tf
+++ dns.tf
@@ -9,13 +9,13 @@ resource "random_string" "name" {
 
 # Create DNS zone
 resource "azurerm_private_dns_zone" "db_dns" {
-  name                = "${random_string.name.result}.${azurerm_mysql_flexible_server.wordpress.name}"
+  name                = "${random_string.name.result}.server.wordpress.com"
   resource_group_name = azurerm_resource_group.azp_rg.name
 }
 
 # Create DNS virtual network line
 resource "azurerm_private_dns_zone_virtual_network_link" "db_dns_link" {
-  name                  = "mysqlfsVnetZone${random_string.name.result}.com"
+  name                  = "azpmysql${random_string.name.result}.com"
   private_dns_zone_name = azurerm_private_dns_zone.db_dns.name
   resource_group_name   = azurerm_resource_group.azp_rg.name
   virtual_network_id    = azurerm_virtual_network.ap_vnet.id
