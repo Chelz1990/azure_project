e5ecf0862b5b2856fc352e150ee060010c5c270b
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-02 09:24:44 -0500

    db

--- database.tf
+++ database.tf
@@ -1,4 +1,8 @@
-
+resource "azurerm_mysql_flexible_server" "wordpress" {
+  name                   = "azp.project.wordpress.com"
+  resource_group_name    = azurerm_resource_group.azp_rg.name
+  location               = azurerm_resource_group.azp_rg.location
+}
 
 #  depends_on = [azurerm_private_dns_zone_virtual_network_link.db_dns_link]
   
