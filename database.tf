9b45cf8e9424d2b9b79bd1506f0a13c22e0c77c5
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2023-12-02 09:26:27 -0500

    edited db

--- database.tf
+++ database.tf
@@ -1,5 +1,5 @@
 #  Create random name 
-resource "random_string" "name" {
+resource "random_string" "dbname" {
   length  = 6
   lower   = true
   numeric = false
@@ -8,7 +8,7 @@ resource "random_string" "name" {
 }
 
 resource "azurerm_mysql_flexible_server" "wordpress" {
-  name                   = "${ransom_string.name.result}azpproject.wordpress.com"
+  name                   = "${ransom_string.dbname.result}azpproject.wordpress.com"
   resource_group_name    = azurerm_resource_group.azp_rg.name
   location               = azurerm_resource_group.azp_rg.location
 }
